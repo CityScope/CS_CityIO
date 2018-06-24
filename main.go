f22e65388ca0d882b51217cfb0ee8fe8a27e96a8
Yasushi Sakai <ysshski@gmail.com>
2018-06-23 20:58:13 -0400

    added root endpoint for redirect

--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"encoding/json"
-	"log"
-	"net/http"
-
 	"github.com/CityScope/CS_CityIO_Backend/models"
 	"github.com/labstack/echo"
+	"log"
+	"net/http"
 )
 
 func main() {
-
+	port := "8080"
 	prefix := "/api"
 	router := echo.New()
 	tables := make(map[string]interface{})
 
+	///////////////////////////////////////
+	router.GET("/", func(c echo.Context) error {
+		return c.Redirect(http.StatusMovedPermanently, "http://cityscope.media.mit.edu/CS_CityIO_Frontend/")
+	})
 	///////////////////////////////////////
 	router.POST(prefix+"/table/update/:tableName", func(c echo.Context) error {
 		data := make(map[string]interface{})
@@ -44,7 +47,6 @@ func main() {
 			map[string]string{"tableName": "done"})
 
 	})
-
 	///////////////////////////////////////
 	router.GET(prefix+"/table/clear/:tableName", func(c echo.Context) error {
 		tableName := c.Param("tableName")
@@ -54,7 +56,6 @@ func main() {
 		return c.JSON(http.StatusOK,
 			map[string]string{"status": "deleted " + tableName})
 	})
-
 	///////////////////////////////////////
 	router.GET(prefix+"/table/:tableName", func(c echo.Context) error {
 		tableName := c.Param("tableName")
@@ -66,7 +67,6 @@ func main() {
 				map[string]string{"status": "table not found"})
 		}
 	})
-
 	///////////////////////////////////////
 	router.GET(prefix+"/tables/list", func(c echo.Context) error {
 		tableList := make([]string, 0, len(tables))
@@ -75,6 +75,5 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, tableList)
 	})
-
-	log.Fatal(router.Start(":8081"))
+	log.Fatal(router.Start(":" + port))
 }
