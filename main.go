635a85f39482d70412816f52c95c6eb25888e9d0
Yasushi Sakai <ysshski@gmail.com>
2018-11-14 06:55:17 -0500

    rudimentary logging

--- main.go
+++ main.go
@@ -2,18 +2,45 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/CityScope/CS_CityIO_Backend/models"
+	"github.com/google/logger"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 func main() {
+
+	t := time.Now()
+	time := fmt.Sprintf("%d-%02d-%02d_%02d:%02d:%02d",
+		t.Year(),
+		t.Month(),
+		t.Day(),
+		t.Hour(),
+		t.Minute(),
+		t.Second())
+
+	logPath := fmt.Sprintf("./log/cityio_%v.log", time)
+	lf, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+
+	if err != nil {
+		logger.Fatalf("Failed to open log file: %v", err)
+	}
+	defer lf.Close()
+
+	defer logger.Init("Logger", false, false, lf).Close()
+
+	//////////////////////////////////////////////
+
 	port := "8080"
 	prefix := "/api"
 	router := echo.New()
+
 	router.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{
@@ -22,21 +49,25 @@ func main() {
 			echo.HeaderAccept},
 		AllowMethods: []string{echo.GET, echo.POST},
 	}))
+
 	tables := make(map[string]interface{})
 
 	///////////////////////////////////////
 	router.GET("/", func(c echo.Context) error {
+		logger.Info("GET request /")
 		return c.Redirect(http.StatusMovedPermanently, "http://cityscope.media.mit.edu/CS_CityIO_Frontend/")
 	})
 	///////////////////////////////////////
 	router.POST(prefix+"/table/update/:tableName", func(c echo.Context) error {
 		data := make(map[string]interface{})
 		tableName := c.Param("tableName")
+		logger.Infof("POST /table/update/%v", tableName)
 
 		err := json.NewDecoder(c.Request().Body).Decode(&data)
 
 		if err != nil {
 			log.Printf("error: %v\n", err.Error())
+			logger.Errorf("error decoding table data from json: %v\n")
 		}
 
 		byteData, _ := json.Marshal(data)
@@ -54,6 +85,7 @@ func main() {
 			tables[tableName] = table
 		}
 
+		logger.Info("POST SUCCESS")
 		return c.JSON(http.StatusOK,
 			map[string]string{"tableName": "done"})
 
@@ -61,6 +93,7 @@ func main() {
 	///////////////////////////////////////
 	router.GET(prefix+"/table/clear/:tableName", func(c echo.Context) error {
 		tableName := c.Param("tableName")
+		logger.Infof("GET /table/clear%v", tableName)
 
 		//TODO: do we want to delete it? perhaps inactivate it?
 		delete(tables, tableName)
@@ -70,16 +103,21 @@ func main() {
 	///////////////////////////////////////
 	router.GET(prefix+"/table/:tableName", func(c echo.Context) error {
 		tableName := c.Param("tableName")
+		logger.Infof("GET /table/%v", tableName)
 		table, ok := tables[tableName]
+
 		if ok {
+			logger.Infof("  found table: %v", tableName)
 			return c.JSON(http.StatusOK, table)
 		} else {
+			logger.Infof("  could not find table: %v", tableName)
 			return c.JSON(http.StatusNotFound,
 				map[string]string{"status": "table not found"})
 		}
 	})
 	///////////////////////////////////////
 	router.GET(prefix+"/tables/list", func(c echo.Context) error {
+		logger.Info("GET /table/list")
 		tableList := make([]string, 0, len(tables))
 		baseUrl := "https://cityio.media.mit.edu/api/table/"
 		for k := range tables {
@@ -87,5 +125,6 @@ func main() {
 		}
 		return c.JSON(http.StatusOK, tableList)
 	})
-	log.Fatal(router.Start(":" + port))
+	logger.Info("server started")
+	logger.Fatal(router.Start(":" + port))
 }
