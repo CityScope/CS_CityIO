170ded172e2fecc5c5d7411edfc0182a10997fad
Alexandra Berke <berke.alexandra@gmail.com>
2018-11-15 12:14:53 -0500

    broke http endpoint functionality into named functions for unit testing
    next step: unit tests

--- main.go
+++ main.go
@@ -14,6 +14,111 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+var port string = "8080"
+var prefix string = "/api"
+
+
+var tables map[string]interface{}
+
+
+
+// GET "/"
+func getFrontend(c echo.Context) error {
+	logger.Info("GETfrontend")
+	return c.Redirect(http.StatusMovedPermanently, "http://cityscope.media.mit.edu/CS_CityIO_Frontend/")
+}
+
+// GET "/api"
+func getAPI(c echo.Context) error {
+	logger.Info("GET request " + prefix)
+	return c.JSON(http.StatusOK, "OK")
+}
+
+// GET "/api/tables/list"
+func listTables(c echo.Context) error {
+	logger.Info("GET /table/list")
+	tableList := make([]string, 0, len(tables))
+	baseUrl := "https://cityio.media.mit.edu/api/table/"
+	for k := range tables {
+		tableList = append(tableList, baseUrl+k)
+	}
+	return c.JSON(http.StatusOK, tableList)
+}
+
+// GET "api/table/:tableName"
+func getTable(c echo.Context) error {
+	tableName := c.Param("tableName")
+	logger.Infof("GET /table/%v", tableName)
+	table, ok := tables[tableName]
+
+	if ok {
+		logger.Infof("  found table: %v", tableName)
+		return c.JSON(http.StatusOK, table)
+	} else {
+		logger.Infof("  could not find table: %v", tableName)
+		return c.JSON(http.StatusNotFound,
+			map[string]string{"status": "table not found"})
+	}
+}
+
+// GET "api/table/clear/:tableName"
+func clearTable(c echo.Context) error {
+	tableName := c.Param("tableName")
+	logger.Infof("GET /table/clear%v", tableName)
+	//TODO: do we want to delete it? perhaps inactivate it?
+	delete(tables, tableName)
+	return c.JSON(http.StatusOK,
+		map[string]string{"status": "deleted " + tableName})
+}
+
+// POST "api/table/update/:tableName"
+func postTable(c echo.Context) error {
+	data := make(map[string]interface{})
+	tableName := c.Param("tableName")
+	logger.Infof("POST /table/update/%v", tableName)
+
+	err := json.NewDecoder(c.Request().Body).Decode(&data)
+
+	if err != nil {
+		log.Printf("error: %v\n", err.Error())
+		logger.Errorf("error decoding table data from json: %v\n", err.Error())
+	}
+
+	byteData, _ := json.Marshal(data)
+
+	table := models.Table{}
+
+	err = json.Unmarshal(byteData, &table)
+	if err != nil {
+		log.Printf("[%v]: invalid type: %v\n", tableName, err.Error())
+		tables[tableName] = data
+	} else {
+		log.Printf("[%v]: valid type \n", tableName)
+
+		hash := table.Hash()
+		update := true
+
+		// don't update when the hash is the same
+		if lastTable, ok := tables[tableName]; ok {
+			lt, yep := lastTable.(models.Table)
+			if yep && hash == lt.Meta.Id {
+				update = false
+			}
+		}
+
+		if update {
+			table.Qualify(hash)
+			tables[tableName] = table
+		}
+	}
+
+	logger.Info("POST SUCCESS")
+	return c.JSON(http.StatusOK,
+		map[string]string{"tableName": "done"})
+
+}
+
+
 func main() {
 
 	t := time.Now()
@@ -37,9 +142,9 @@ func main() {
 
 	//////////////////////////////////////////////
 
-	port := "8080"
-	prefix := "/api"
 	router := echo.New()
+
+	tables = make(map[string]interface{})
 
 	router.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
@@ -50,94 +155,22 @@ func main() {
 		AllowMethods: []string{echo.GET, echo.POST},
 	}))
 
-	tables := make(map[string]interface{})
 
-	///////////////////////////////////////
-	router.GET("/", func(c echo.Context) error {
-		logger.Info("GET request /")
-		return c.Redirect(http.StatusMovedPermanently, "http://cityscope.media.mit.edu/CS_CityIO_Frontend/")
-	})
-	///////////////////////////////////////
-	router.POST(prefix+"/table/update/:tableName", func(c echo.Context) error {
-		data := make(map[string]interface{})
-		tableName := c.Param("tableName")
-		logger.Infof("POST /table/update/%v", tableName)
+	// Frontend redirects to city IO frontend
+	router.GET("/", getFrontend)
 
-		err := json.NewDecoder(c.Request().Body).Decode(&data)
+	// API endpoints
 
-		if err != nil {
-			log.Printf("error: %v\n", err.Error())
-			logger.Errorf("error decoding table data from json: %v\n")
-		}
+	router.GET(prefix, getAPI)
 
-		byteData, _ := json.Marshal(data)
+	router.POST(prefix+"/table/update/:tableName", postTable)
+	
+	router.GET(prefix+"/table/clear/:tableName", clearTable)
+	
+	router.GET(prefix+"/table/:tableName", getTable)
 
-		table := models.Table{}
+	router.GET(prefix+"/tables/list", listTables)
 
-		err = json.Unmarshal(byteData, &table)
-		if err != nil {
-			log.Printf("[%v]: invalid type: %v\n", tableName, err.Error())
-			tables[tableName] = data
-		} else {
-			log.Printf("[%v]: valid type \n", tableName)
-
-			hash := table.Hash()
-			update := true
-
-			// don't update when the hash is the same
-			if lastTable, ok := tables[tableName]; ok {
-				lt, yep := lastTable.(models.Table)
-				if yep && hash == lt.Meta.Id {
-					update = false
-				}
-			}
-
-			if update {
-				table.Qualify(hash)
-				tables[tableName] = table
-			}
-		}
-
-		logger.Info("POST SUCCESS")
-		return c.JSON(http.StatusOK,
-			map[string]string{"tableName": "done"})
-
-	})
-	///////////////////////////////////////
-	router.GET(prefix+"/table/clear/:tableName", func(c echo.Context) error {
-		tableName := c.Param("tableName")
-		logger.Infof("GET /table/clear%v", tableName)
-
-		//TODO: do we want to delete it? perhaps inactivate it?
-		delete(tables, tableName)
-		return c.JSON(http.StatusOK,
-			map[string]string{"status": "deleted " + tableName})
-	})
-	///////////////////////////////////////
-	router.GET(prefix+"/table/:tableName", func(c echo.Context) error {
-		tableName := c.Param("tableName")
-		logger.Infof("GET /table/%v", tableName)
-		table, ok := tables[tableName]
-
-		if ok {
-			logger.Infof("  found table: %v", tableName)
-			return c.JSON(http.StatusOK, table)
-		} else {
-			logger.Infof("  could not find table: %v", tableName)
-			return c.JSON(http.StatusNotFound,
-				map[string]string{"status": "table not found"})
-		}
-	})
-	///////////////////////////////////////
-	router.GET(prefix+"/tables/list", func(c echo.Context) error {
-		logger.Info("GET /table/list")
-		tableList := make([]string, 0, len(tables))
-		baseUrl := "https://cityio.media.mit.edu/api/table/"
-		for k := range tables {
-			tableList = append(tableList, baseUrl+k)
-		}
-		return c.JSON(http.StatusOK, tableList)
-	})
 	logger.Info("server started")
 	logger.Fatal(router.Start(":" + port))
 }
