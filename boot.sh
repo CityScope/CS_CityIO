99c56a92000db189407679c5e62f98c069636dfc
Ariel Noyman <noyman@mit.edu>
2019-03-11 14:31:12 -0400

    trying to fix crash on restart using slow startup

--- boot.sh
+++ boot.sh
@@ -6,13 +6,12 @@ sudo chown root:wheel /usr/local/opt/nginx/homebrew.mxcl.nginx.plist
 sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
 
 echo "starting nginx server"
-
+sleep 0.5
+echo "started cityio..."
 tmux new -d -s cityio '/Users/yasushi/code/go/bin/CS_CityIO_Backend'
-
-echo "started cityio"
-
+# GO to sleep 
+echo "started choiceModels in a few seconds.."
+sleep 2
 tmux new -d -s choiceModels '/Users/Shared/CS_choiceModels/runCM.sh'
-
-echo "started choiceModels"
 
 
