52e67dba4c0bf0222ed101e67a6e410a6132dc17
Yasushi Sakai <ysshski@gmail.com>
2019-03-19 11:00:34 -0400

    ravised boot.sh

--- boot.sh
+++ boot.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
+if [ "$(whoami)" != "root" ]; then
+  echo "please RUN boot.sh with SUDO"
+  exit 1
+fi
+
 # boot nginx
 sudo ln -s /usr/local/opt/nginx/homebrew.mxcl.nginx.plist /Library/LaunchDaemons/
 sudo chown root:wheel /usr/local/opt/nginx/homebrew.mxcl.nginx.plist
 sudo launchctl load /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
 
+tmux -S /tmp/shared kill-server
+
 echo "starting nginx server"
 sleep 0.5
-echt "started cityio..."
-tmux new -d -S /tmp/cityio-session cityio '/Users/yasushi/code/go/bin/CS_CityIO_Backend'
-chmod 777 /tmp/cityio-session
-# GO to sleep 
-echo "started choiceModels in a few seconds.."
-sleep 2
-tmux new -d -S /tmp/choiceModels-session choiceModels '/Users/Shared/CS_choiceModels/runCM.sh'
-chmod 777 /tmp/choiceModels-session
 
+tmux -S /tmp/shared new-session -d  -s cityio
+tmux -S /tmp/shared send-keys '/Users/yasushi/code/go/bin/CS_CityIO_Backend' C-m
+# tmux -S /tmp/shared detach -s cityio
+echo "started cityio... attach using following command"
+echo "tmux -S /tmp/shared a -t cityio"
+
+sleep 2
+
+tmux -S /tmp/shared new-session -d  -s choiceModels
+tmux -S /tmp/shared send-keys '/Users/Shared/CS_choiceModels/runCM.sh' C-m
+# jtmux -S /tmp/shared detach -s choiceModels
+echo "started mocho..."
+echo "tmux -S /tmp/shared a -t choiceModels"
+
+chmod 777 /tmp/shared
 
