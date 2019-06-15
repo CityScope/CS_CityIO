d8eabcfb3931418701f48263c92bc43ba8fd24ae
yasushisakai <ysshski@gmail.com>
2019-06-15 10:54:25 -0400

    changed 'meta' field

--- src/model.rs
+++ src/model.rs
@@ -13,11 +13,12 @@ pub type JSONObject = Map<String, Value>;
 #[derive(Serialize, Deserialize)]
 pub struct TableList(Vec<String>);
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct Meta {
     id: String,
     timestamp: u64,
     apiv: String,
+    pub hashes: HashMap<String, String>
 }
 
 impl Meta {
@@ -33,7 +34,44 @@ impl Meta {
             id: id,
             timestamp: now,
             apiv: apiv,
+            hashes: HashMap::<String, String>::new()
         }
+    }
+
+    pub fn from_map(map: &JSONObject) -> Meta {
+
+        let mut hmap = HashMap::<String, String>::new();
+       
+        for key in map.keys() {
+            if key == "meta" {
+                continue;
+            }
+            let h = format_hash(&hash(&map.get(key).unwrap().to_string()));
+            hmap.insert(key.to_owned(), h);
+        }
+
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let apiv = String::from("2.1");
+
+        Meta{
+            id: "id".to_owned(),
+            timestamp: now,
+            apiv: apiv,
+            hashes: hmap
+        }
+    }
+
+    pub fn update(&mut self) {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        self.timestamp = now;
     }
 
     pub fn id(&self) -> String {
