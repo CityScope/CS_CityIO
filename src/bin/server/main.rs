8ebfe2235724953740629f621361364d36fa8407
Yasushi Sakai <ysshski@gmail.com>
2019-11-20 13:29:42 -0500

    CORS:*

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -97,11 +97,12 @@ fn main() -> std::io::Result<()> {
                 Cors::new()
                     // allowing this for debug purposes
                     // TODO: turn this off at production
-                    .allowed_origin("http://localhost:4200")
-                    .allowed_origin("http://localhost:5500")
-                    .allowed_origin("http://localhost:1234")
-                    .allowed_origin("https://github.com")
-                    .allowed_origin("https://cityio.media.mit.edu")
+                    // .allowed_origin("http://localhost:4200")
+                    // .allowed_origin("http://localhost:5500")
+                    // .allowed_origin("http://localhost:1234")
+                    // .allowed_origin("https://github.com")
+                    // .allowed_origin("https://cityio.media.mit.edu")
+                    // .allowed_origin("https://cityscope.media.mit.edu")
                     .allowed_methods(vec!["GET", "POST"])
                     .send_wildcard()
                     .allowed_headers(vec![
